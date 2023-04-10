57588bea7c19c94abf48929e5080d36235933be2
Cory Cox <corycox@Corys-MBP.lan>
2023-04-10 13:34:06 -0400

    deploy change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.querySelector("#root")
 );
